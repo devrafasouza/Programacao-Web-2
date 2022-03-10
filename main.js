3a8738924ba4bbc0a4adfeae9076af78002cf023
Rafael Alcântara <devrafasouza@gmail.com>
2022-03-10 13:55:59 -0300

    servidor rodando e gitignore

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ const http = require('http'); /* modulo http */
 const port = 3000; /* numero da porta */
 
 const server = http.createServer((request, response) => {
-  response.end("<h1>Olá Mundo</h1>");
+  response.end("<h1>Ola Mundo</h1>");
 }); /* cria o server e passa de callback uma resposta para o usuario */
 
+server.listen(port, () => {
+  console.log("Ouvindo na porta " + port);
+});
